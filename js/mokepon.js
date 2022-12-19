087b0b2a66d2fa9fa65769b65d50fa3af6d83abc
sariswis <ss.cardenas@uniandes.edu.co>
2022-12-19 18:33:30 -0500

    Optimización

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,6 @@
 const select_pet = document.getElementById('select_pet')
 const pet_cards = document.getElementById('pet_cards')
-const input_pets = document.getElementsByClassName('pet')
+const input_pets = document.getElementsByName('pet')
 const btn_select_pet = document.getElementById('btn_select_pet')
 
 const select_attack = document.getElementById('select_attack')
@@ -19,82 +19,41 @@ const p_enemy_pet = document.getElementById('p_enemy_pet')
 const history_player = document.getElementById('history_player')
 const history_enemy = document.getElementById('history_enemy')
 
-var lt_mokepones = [], dict_mokepones = new Map()
-var player, enemy, player_attacks, enemy_attacks
-var real_p_attacks = new Map(), real_e_attacks = new Map() 
+const lt_mokepones = [], dict_mokepones = new Map()
+const real_p_attacks = new Map(), real_e_attacks = new Map() 
+var player, enemy, player_i, enemy_i
+var player_lives, enemy_lives, player_attacks, enemy_attacks
 var p_attack, e_attack, player_attack, enemy_attack
-var player_lives = 5, enemy_lives = 5
+
 window.addEventListener('load', loadGame)
 
-class Mokepon {
-    constructor (name, type, lives) {
-        this.name = name
-        this.type = type
-        this.image = `assets/${name}.svg`
-        this.lives = lives
-        this.attacks = []
-    }
-}
-
-var cattie = new Mokepon('Cattie', 'Fire 🔥', 4)
-var doggito = new Mokepon('Doggito', 'Water 💧', 4)
-var lapinette = new Mokepon('Lapinette', 'Soil 🌱', 4)
-
-cattie.attacks.push(
-    {'name':'Ball 🧶', 'id':'btn_ball', 'real_name':'Fire 🔥'},
-    {'name':'Fish 🐟', 'id':'btn_fish', 'real_name':'Fire 🔥'},
-    {'name':'Fire 🔥', 'id':'btn_fire', 'real_name':'Fire 🔥'},
-    {'name':'Water 💧', 'id':'btn_water', 'real_name':'Water 💧'},
-    {'name':'Soil 🌱', 'id':'btn_soil', 'real_name':'Soil 🌱'}
-    )
-
-doggito.attacks.push(
-    {'name':'Bone 🦴', 'id':'btn_bone', 'real_name':'Water 💧'},
-    {'name':'Bark 🐶', 'id':'btn_bark', 'real_name':'Water 💧'},
-    {'name':'Fire 🔥', 'id':'btn_fire', 'real_name':'Fire 🔥'},
-    {'name':'Water 💧', 'id':'btn_water', 'real_name':'Water 💧'},
-    {'name':'Soil 🌱', 'id':'btn_soil', 'real_name':'Soil 🌱'}
-    )
-
-lapinette.attacks.push(
-    {'name':'Carrot 🥕', 'id':'btn_carrot', 'real_name':'Soil 🌱'},
-    {'name':'Teeth 🦷', 'id':'btn_teeth', 'real_name':'Soil 🌱'},
-    {'name':'Fire 🔥', 'id':'btn_fire', 'real_name':'Fire 🔥'},
-    {'name':'Water 💧', 'id':'btn_water', 'real_name':'Water 💧'},
-    {'name':'Soil 🌱', 'id':'btn_soil', 'real_name':'Soil 🌱'}
-    )
-
-lt_mokepones.push(cattie, doggito, lapinette)
-
 function loadGame(){
-    p_player_lives.innerHTML = player_lives
-    p_enemy_lives.innerHTML = enemy_lives
-    btn_select_pet.addEventListener('click', selectPets)
-
     select_attack.style.display = 'none'
     messages.style.display = 'none'
     btn_restart.style.display = 'none'
+    loadData()
 
     lt_mokepones.forEach((mokepon) => {
-        pet = `<input type='radio' class='pet' id=${mokepon.name}>
+        pet = `<input type='radio' name='pet' id=${mokepon.name}>
                 <label class='pet_card' for=${mokepon.name}>
-                    <p>${mokepon.name}</p>
+                    <p>${mokepon.name} ${mokepon.type}</p>
                     <img src=${mokepon.image} alt=${mokepon.name}>
                 </label>`
         pet_cards.innerHTML += pet
         dict_mokepones.set(mokepon.name, mokepon)
     })
+
+    btn_select_pet.addEventListener('click', selectPets)
 }
 
 function selectPets(){
     for(pet of input_pets){
         if (pet.checked){
             player = pet.id, enemy = input_pets[random(0, input_pets.length - 1)].id
-            p_player_pet.innerHTML = player, p_enemy_pet.innerHTML = enemy
-            
-            player_logo.src = `assets/${player}.svg`, player_logo.alt = player
-            enemy_logo.src = `assets/${enemy}.svg`, enemy_logo.alt = enemy
-            getAttacks(player, enemy)
+            player_i = dict_mokepones.get(player), enemy_i = dict_mokepones.get(enemy)
+            player_lives = player_i.lives, enemy_lives = enemy_i.lives
+            player_attacks = player_i.attacks, enemy_attacks = enemy_i.attacks
+            putInfo()
 
             select_pet.style.display = 'none'
             select_attack.style.display = 'flex'
@@ -107,14 +66,19 @@ function selectPets(){
     }
 }
 
-function getAttacks(player, enemy){
-    player_attacks = dict_mokepones.get(player).attacks
-    enemy_attacks = dict_mokepones.get(enemy).attacks
+function putInfo(){
     player_attacks.forEach((attack) => {
         attack_buttons.innerHTML += `<button id=${attack.id} class='btn_attack'>${attack.name}</button>`
         real_p_attacks.set(attack.name, attack.real_name)
     })
     enemy_attacks.forEach((attack) => {real_e_attacks.set(attack.name, attack.real_name)})
+    
+    p_player_lives.innerHTML = player_lives
+    p_enemy_lives.innerHTML = enemy_lives
+    p_player_pet.innerHTML = `${player} ${player_i.type}`
+    p_enemy_pet.innerHTML = `${enemy} ${enemy_i.type}` 
+    player_logo.src = `assets/${player}.svg`, player_logo.alt = player
+    enemy_logo.src = `assets/${enemy}.svg`, enemy_logo.alt = enemy
 }
 
 function fight(event){
@@ -122,7 +86,6 @@ function fight(event){
     e_attack = enemy_attacks[random(0, enemy_attacks.length - 1)].name
     player_attack = real_p_attacks.get(p_attack), enemy_attack = real_e_attacks.get(e_attack)
     console.log(player_attack, enemy_attack)
-    /* enemy_attack = attacks[random(0, attacks.length - 1)].innerHTML */
 
     if ((player_attack == enemy_attack)){
         result = `It's a tie!`
@@ -181,4 +144,39 @@ function reload(){
 
 function random(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function loadData(){
+    class Mokepon {
+        constructor (name, type, attacks, lives) {
+            this.name = name
+            this.type = type
+            this.attacks = attacks
+            this.image = `assets/${name}.svg`
+            this.lives = lives
+        }
+    }
+
+    let cattie_attacks = [{'name':'Ball 🧶', 'id':'btn_ball', 'real_name':'Fire 🔥'},
+                        {'name':'Fish 🐟', 'id':'btn_fish', 'real_name':'Fire 🔥'},
+                        {'name':'Fire 🔥', 'id':'btn_fire', 'real_name':'Fire 🔥'},
+                        {'name':'Water 💧', 'id':'btn_water', 'real_name':'Water 💧'},
+                        {'name':'Soil 🌱', 'id':'btn_soil', 'real_name':'Soil 🌱'}]
+
+    let doggito_attacks = [{'name':'Bone 🦴', 'id':'btn_bone', 'real_name':'Water 💧'},
+                        {'name':'Bark 🐶', 'id':'btn_bark', 'real_name':'Water 💧'},
+                        {'name':'Fire 🔥', 'id':'btn_fire', 'real_name':'Fire 🔥'},
+                        {'name':'Water 💧', 'id':'btn_water', 'real_name':'Water 💧'},
+                        {'name':'Soil 🌱', 'id':'btn_soil', 'real_name':'Soil 🌱'}]
+
+    let lapinette_attacks = [{'name':'Carrot 🥕', 'id':'btn_carrot', 'real_name':'Soil 🌱'},
+                        {'name':'Teeth 🦷', 'id':'btn_teeth', 'real_name':'Soil 🌱'},
+                        {'name':'Fire 🔥', 'id':'btn_fire', 'real_name':'Fire 🔥'},
+                        {'name':'Water 💧', 'id':'btn_water', 'real_name':'Water 💧'},
+                        {'name':'Soil 🌱', 'id':'btn_soil', 'real_name':'Soil 🌱'}]
+
+    let cattie = new Mokepon('Cattie', '🔥', cattie_attacks, 4)
+    let doggito = new Mokepon('Doggito', '💧', doggito_attacks, 4)
+    let lapinette = new Mokepon('Lapinette', '🌱', lapinette_attacks, 4)
+    lt_mokepones.push(cattie, doggito, lapinette)
 }
